0dcbdf68fbb7cece1c659a1072fec64526b357c5
Daniyal Khan <dkay7223@gmail.com>
2023-12-15 11:24:39 +0500

    Add files via upload

---
+++ predictor.py
@@ -0,0 +1,93 @@
+import pickle
+
+from flask import Flask, jsonify, request
+from flask_cors import CORS
+import tensorflow as tf
+from tensorflow.keras.models import load_model
+from tensorflow.python.keras import activations
+from transformers import TFDistilBertForSequenceClassification, DistilBertTokenizer
+import numpy as np
+import json
+import nltk
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+from nltk.stem import PorterStemmer
+import string
+
+nltk.download('punkt')
+nltk.download('stopwords')
+
+def construct_encodings(x, tokenizer, max_len, trucation=True, padding=True):
+  return tokenizer(x, max_length=max_len, truncation=trucation, padding=padding)
+
+def construct_tfdataset(encodings, y=None):
+    if y:
+      return tf.data.Dataset.from_tensor_slices((dict(encodings), y))
+    else:
+      # this case is used when making predictions on unseen samples after training
+      return tf.data.Dataset.from_tensor_slices(dict(encodings))
+
+def create_predictor(model, model_name, max_len):
+  tokenizer = DistilBertTokenizer.from_pretrained(model_name)
+  def predict_proba(text):
+      x = [text]
+
+      encodings = construct_encodings(x, tokenizer, max_len=max_len)
+      tfdataset = construct_tfdataset(encodings)
+      tfdataset = tfdataset.batch(1)
+
+      preds = model.predict(tfdataset).logits
+      preds = activations.softmax(tf.convert_to_tensor(preds)).numpy()
+      return preds[0][1]
+
+  return predict_proba
+app = Flask(__name__)
+CORS(app)
+new_model = TFDistilBertForSequenceClassification.from_pretrained('C:/Users/mu393/Downloads/sentiment_analyzer_model_new/sentiment_analyzer_model')
+model_name, max_len = pickle.load(open('C:/Users/mu393/Downloads/sentiment_analyzer_model_new/sentiment_analyzer_model/info.pkl', 'rb'))
+
+model = create_predictor(new_model, model_name,max_len)
+print(model)
+
+
+def preprocess(text):
+    text = text.lower()
+    
+    # Tokenization
+    tokens = word_tokenize(text)
+    
+    # Remove punctuation
+    tokens = [word for word in tokens if word not in string.punctuation]
+    
+    # Remove stopwords
+    stop_words = set(stopwords.words('english'))
+    tokens = [word for word in tokens if word not in stop_words]
+    
+    # Stemming (using Porter Stemmer)
+    stemmer = PorterStemmer()
+    tokens = [stemmer.stem(word) for word in tokens]
+    
+    # Join the tokens back into a single string
+    processed_text = ' '.join(tokens)
+    
+    return processed_text
+
+@app.route('/predict', methods=['POST'])
+def predict_sentiment():
+    try:
+        
+        # Get input data from the request
+        data = request.json
+        text = data['text']
+        text = preprocess(text)
+        prediction = model(text)
+        # Return the prediction as JSON
+        return jsonify({'score': float(prediction)})
+    except Exception as e:
+        return jsonify({'error': str(e)})
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
+
